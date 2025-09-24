c4ac225a871961dca1425ec40793ed290393df90
Swapnaja27 <142212367+Swapnaja27@users.noreply.github.com>
2025-09-24 11:19:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 function getFormvalue() {
     //Write your code here
+    const firstName = document.querySelector("input[name='fname']").value.trim();
+      const lastName = document.querySelector("input[name='lname']").value.trim();
 
+      // Handle empty fields
+      if (!firstName && !lastName) {
+        alert("Please enter both First and Last Name!");
+        return false;
+      }
+
+      // Concatenate full name
+      const fullName = `${firstName} ${lastName}`.trim();
+
+      // Show alert
+      alert(fullName);
+
+      // Prevent form from reloading the page
+      return false;
 }
+document.getElementByTag("button").addEventListener("click",()=>{
+	getFormvalue();
+})
